f042b02e3da3366dddefe9ac57d294d1ec9201a6
Adrianna <adriannalenn@gmail.com>
2024-02-28 18:51:59 +0100

    adding email validation

--- script.js
+++ script.js
@@ -1,15 +1,76 @@
 
-document.getElementById("subscribe").addEventListener("click", function() {
+// document.getElementById("subscribe").addEventListener("click", function() {
 
-    document.getElementsByClassName("subscription__window")
-    [0].classList.add("active");
+//     document.getElementsByClassName("subscription__window")
+//      [0].classList.add("active");
 
-})
+// })
+
+// document.getElementById("dissmiss__popout__btn").addEventListener("click", function() {
+
+//     document.getElementsByClassName("subscription__window")
+//     [0].classList.remove("active");
+    
+//  })
+
+
+// Get references to the input field, error message, and the subscribe button
+const emailInput = document.getElementById('emailInput');
+const error = document.querySelector('.error');
+const subscribeButton = document.getElementById('subscribe');
+
+// Function to validate email
+function validateEmail(email) {
+    // Regular expression to validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+// Function to handle subscription
+function subscribe() {
+    const email = emailInput.value; //zwraca string
+
+    // Validate email format
+    if (!validateEmail(email)) {
+        // Display error message if email is invalid
+
+       
+        return; // Exit function if email is invalid
+    }
+
+    else { 
+
+        document.getElementsByClassName("subscription__window")
+         [0].classList.add("active");
+        
+         mainContent.style.display = "none";
+     }
+
+    // Clear the input field after subscription
+    emailInput.value = '';
+   
+}
 
 document.getElementById("dissmiss__popout__btn").addEventListener("click", function() {
 
     document.getElementsByClassName("subscription__window")
     [0].classList.remove("active");
+
+    mainContent.style.display = "block";
     
-})
+ })
+
+
+// Event listener for the subscribe button click
+subscribeButton.addEventListener('click', subscribe);
+
+
+// Event listener for the Enter key press in the input field to trigger subscription
+emailInput.addEventListener('keypress', function(event) {
+    // Check if the Enter key was pressed
+    if (event.key === 'Enter') {
+        subscribe();
+    }
+});
+
 
